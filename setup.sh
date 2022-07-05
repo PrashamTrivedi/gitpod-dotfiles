702f3c419c7a57dd36f15b77eb13ef886d435ce2
Prasham Trivedi <PrashamTrivedi@users.noreply.github.com>
2022-07-05 12:54:39 +0530

    Adding Environment Variables without export

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 curl -L "https://github.com/PrashamTrivedi/gitconfig-provider/releases/download/1.21/gitconfig-provider_1.21_Linux_x86_64.tar.gz" -o "gitconfig-provider.tar.gz"
 tar -xvf gitconfig-provider.tar.gz
 chmod +x gitconfig-provider
-export PATH="$HOME/gitconfig-provider:$PATH"
+PATH="$HOME/gitconfig-provider:$PATH"
 
 gitconfig-provider addConfig -provider=Github -key=user.email -value=$GITHUB_USER_EMAIL
 gitconfig-provider addConfig -provider=Github -key=user.name -value=$GITHUB_USER_NAME
@@ -9,9 +9,9 @@ gitconfig-provider addConfig -provider=Github -key=user.name -value=$GITHUB_USER
 alias gitpull="git stash && git pull && git stash pop"
 alias gitpush="git push && git push --tags && git stash clear"
 
-export AWS_ACCESS_KEY_ID=$AWS_PERSONAL_ACCESS_KEY_ID
-export AWS_SECRET_ACCESS_KEY=$AWS_PERSONAL_SECRET_ACCESS_KEY
-export AWS_DEFAULT_REGION=ap-south-1
+AWS_ACCESS_KEY_ID=$AWS_PERSONAL_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY=$AWS_PERSONAL_SECRET_ACCESS_KEY
+AWS_DEFAULT_REGION=ap-south-1
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
