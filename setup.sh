e8f43bbf2a4d28f286330d79b490bd4c8f22f760
Prasham Trivedi <PrashamTrivedi@users.noreply.github.com>
2022-07-05 12:28:19 +0530

    Updating proper link

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
-curl -L "https://github.com/PrashamTrivedi/gitconfig-provider/releases/download/1.21/gitconfig-provider_1.21_Linux_arm64.tar.gz" -o "gitconfig-provider.tar.gz"
+curl -L "https://github.com/PrashamTrivedi/gitconfig-provider/releases/download/1.21/gitconfig-provider_1.21_Linux_x86_64.tar.gz" -o "gitconfig-provider.tar.gz"
 tar -xvf gitconfig-provider.tar.gz
+chmod +x gitconfig-provider
 
 gitconfig-provider addConfig -provider=Github -key=user.email -value=$GITHUB_USER_EMAIL
 gitconfig-provider addConfig -provider=Github -key=user.name -value=$GITHUB_USER_NAME
