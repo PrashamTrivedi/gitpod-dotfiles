07670db512e6493cce1e7c01cfb5317d00d5ae20
Prasham Trivedi <PrashamTrivedi@users.noreply.github.com>
2022-07-05 12:21:27 +0530

    adding -L to follow redirect for github releases

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-curl "https://github.com/PrashamTrivedi/gitconfig-provider/releases/download/1.21/gitconfig-provider_1.21_Linux_arm64.tar.gz" -o "gitconfig-provider.tar.gz"
+curl -L "https://github.com/PrashamTrivedi/gitconfig-provider/releases/download/1.21/gitconfig-provider_1.21_Linux_arm64.tar.gz" -o "gitconfig-provider.tar.gz"
 tar -xvf gitconfig-provider.tar.gz
 
 gitconfig-provider addConfig -provider=Github -key=user.email -value=$GITHUB_USER_EMAIL
