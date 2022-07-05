bfea3357388eb480209a67c21841562922a37ca9
Prasham Trivedi <PrashamTrivedi@users.noreply.github.com>
2022-07-05 12:35:57 +0530

    Added Gitconfig provider to path

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 curl -L "https://github.com/PrashamTrivedi/gitconfig-provider/releases/download/1.21/gitconfig-provider_1.21_Linux_x86_64.tar.gz" -o "gitconfig-provider.tar.gz"
 tar -xvf gitconfig-provider.tar.gz
 chmod +x gitconfig-provider
+export PATH="$HOME/gitconfig-provider:$PATH"
 
 gitconfig-provider addConfig -provider=Github -key=user.email -value=$GITHUB_USER_EMAIL
 gitconfig-provider addConfig -provider=Github -key=user.name -value=$GITHUB_USER_NAME
