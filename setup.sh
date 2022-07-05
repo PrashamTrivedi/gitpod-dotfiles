84c8cf54594b752d968730c24eca469322b4af0b
PrashamTrivedi <PrashamTrivedi@users.noreply.github.com>
2022-07-05 19:08:09 +0530

    Adding shebang in sh files

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 curl -L "https://github.com/PrashamTrivedi/gitconfig-provider/releases/download/1.21/gitconfig-provider_1.21_Linux_x86_64.tar.gz" -o "gitconfig-provider.tar.gz"
 tar -xvf gitconfig-provider.tar.gz
 chmod +x gitconfig-provider
