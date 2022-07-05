e3b87f781c9f0266c53945d671ccba108499586f
PrashamTrivedi <PrashamTrivedi@users.noreply.github.com>
2022-07-05 18:59:25 +0530

    Adding Environment Variables

--- setup.sh
+++ setup.sh
@@ -1,12 +1,11 @@
 git config user.name $GITHUB_USER_NAME && git config user.email $GITHUB_USER_EMAIL
-alias gitpull="git stash && git pull && git stash pop"
-alias gitpush="git push && git push --tags && git stash clear"
 
-export AWS_ACCESS_KEY_ID=$AWS_PERSONAL_ACCESS_KEY_ID
-export AWS_SECRET_ACCESS_KEY=$AWS_PERSONAL_SECRET_ACCESS_KEY
-export AWS_DEFAULT_REGION=ap-south-1
+
+
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
 aws --version
+
+source environments.sh
