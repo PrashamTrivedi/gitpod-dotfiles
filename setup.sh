ac9c24889f94ba4cb22039d2db629206935cea08
PrashamTrivedi <PrashamTrivedi@users.noreply.github.com>
2022-07-27 11:26:22 +0530

    Renaming bashrc file and adding comments

--- setup.sh
+++ setup.sh
@@ -1,19 +1,28 @@
 #!/bin/sh
+
+# Install Gitconfig Provider
 curl -L "https://github.com/PrashamTrivedi/gitconfig-provider/releases/download/1.21/gitconfig-provider_1.21_Linux_x86_64.tar.gz" -o "gitconfig-provider.tar.gz"
 tar -xvf gitconfig-provider.tar.gz
 chmod +x gitconfig-provider
 
-cp ./gitconfig-provider ~/.bashrc.d/gitconfig-provider
+# Copy gitconfit provider bashrc
+chmod +x gitconfig-provider-bashrc
+cp gitconfig-provider-bashrc ~/.bashrc.d/gitconfig-provider-bashrc
 
+# Apply Gitconfig provider for github
 gitconfig-provider addConfig -provider=Github -key=user.email -value=$GITHUB_USER_EMAIL
 gitconfig-provider addConfig -provider=Github -key=user.name -value=$GITHUB_USER_NAME
 
-
+# Install and verify AWS
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
 aws --version
 
-cp ./aws ~/.bashrc.d/aws
+# Copy aws to Bashrc
+chmod +x aws
+cp aws ~/.bashrc.d/aws
 
-cp ./git-alias ~/.bashrc.d/git-alias
+# Copy git-alias to Bashrc
+chmod +x git-alias
+cp git-alias ~/.bashrc.d/git-alias
