d9ad50de985c6390b9a7615cf4b172244e9b15de
Prasham Trivedi <PrashamTrivedi@users.noreply.github.com>
2022-07-27 11:15:44 +0530

    Adding chunks of env variable to bashrc.d

--- environments.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-export PATH="$HOME/gitconfig-provider:$PATH"
-export AWS_ACCESS_KEY_ID=$AWS_PERSONAL_ACCESS_KEY_ID
-export AWS_SECRET_ACCESS_KEY=$AWS_PERSONAL_SECRET_ACCESS_KEY
-export AWS_DEFAULT_REGION=ap-south-1
-alias gitpull="git stash && git pull && git stash pop"
-alias gitpush="git push && git push --tags && git stash clear"
-ZSH_THEME="agnoster"
--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 curl -L "https://github.com/PrashamTrivedi/gitconfig-provider/releases/download/1.21/gitconfig-provider_1.21_Linux_x86_64.tar.gz" -o "gitconfig-provider.tar.gz"
 tar -xvf gitconfig-provider.tar.gz
 chmod +x gitconfig-provider
-$PATH="gitconfig-provider:$PATH"
 
+cp ./gitconfig-provider ~/.bashrc.d/gitconfig-provider
 
 gitconfig-provider addConfig -provider=Github -key=user.email -value=$GITHUB_USER_EMAIL
 gitconfig-provider addConfig -provider=Github -key=user.name -value=$GITHUB_USER_NAME
@@ -14,10 +14,6 @@ unzip awscliv2.zip
 sudo ./aws/install
 aws --version
 
-. ~/.dotfiles/environments.sh
+cp ./aws ~/.bashrc.d/aws
 
-echo $AWS_DEFAULT_REGION
-# eval $(gp env PATH="$HOME/gitconfig-provider:$PATH")
-# eval $(gp env AWS_ACCESS_KEY_ID=$AWS_PERSONAL_ACCESS_KEY_ID)
-# eval $(gp env AWS_SECRET_ACCESS_KEY=$AWS_PERSONAL_SECRET_ACCESS_KEY)
-# eval $(gp env AWS_DEFAULT_REGION=ap-south-1)
+cp ./git-alias ~/.bashrc.d/git-alias
