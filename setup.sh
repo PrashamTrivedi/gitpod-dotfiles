93ceddbf16237d96deff1f1b29445c3f6420c080
PrashamTrivedi <PrashamTrivedi@users.noreply.github.com>
2022-07-27 12:08:49 +0530

    Absolute Path

--- setup.sh
+++ setup.sh
@@ -13,11 +13,8 @@ echo $(pwd)
 
 # Copy gitconfit provider bashrc
 chmod +x gitconfig-provider-bashrc
-cp .dotfliles/gitconfig-provider-bashrc ~/.bashrc.d/gitconfig-provider-bashrc
+cp ~/.dotfliles/gitconfig-provider-bashrc ~/.bashrc.d/gitconfig-provider-bashrc
 
-# Apply Gitconfig provider for github
-gitconfig-provider addConfig -provider=Github -key=user.email -value=$GITHUB_USER_EMAIL
-gitconfig-provider addConfig -provider=Github -key=user.name -value=$GITHUB_USER_NAME
 
 # Install and verify AWS
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
@@ -31,6 +28,10 @@ cp .dotfliles/aws ~/.bashrc.d/aws
 
 # Copy git-alias to Bashrc
 chmod +x git-alias
-cp .dotfliles/git-alias ~/.bashrc.d/git-alias
+cp ~/.dotfliles/git-alias ~/.bashrc.d/git-alias
 
 echo $(pwd)
+
+# Apply Gitconfig provider for github
+gitconfig-provider addConfig -provider=Github -key=user.email -value=$GITHUB_USER_EMAIL
+gitconfig-provider addConfig -provider=Github -key=user.name -value=$GITHUB_USER_NAME
