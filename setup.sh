34c2d6ac11b370511a96baa88a21809f5811134c
Prasham Trivedi <PrashamTrivedi@users.noreply.github.com>
2022-08-03 20:08:45 +0530

    Updating Gitconfig provider version

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 echo CurrentDir is
 echo $(pwd) 
 
-curl -L "https://github.com/PrashamTrivedi/gitconfig-provider/releases/download/1.21/gitconfig-provider_1.21_Linux_x86_64.tar.gz" -o "gitconfig-provider.tar.gz"
+curl -L "https://github.com/PrashamTrivedi/gitconfig-provider/releases/download/1.22/gitconfig-provider_1.22_Linux_x86_64.tar.gz" -o "gitconfig-provider.tar.gz"
 tar -xvf gitconfig-provider.tar.gz
 chmod +x gitconfig-provider
 
