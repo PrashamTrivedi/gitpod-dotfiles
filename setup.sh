809c714775e8935570e4132ed8dcf114d2fd5c90
PrashamTrivedi <PrashamTrivedi@users.noreply.github.com>
2022-08-04 12:19:30 +0530

    Changing path of script

--- setup.sh
+++ setup.sh
@@ -33,5 +33,5 @@ cp ~/.dotfiles/git-alias ~/.bashrc.d/git-alias
 echo $(pwd)
 
 # Apply Gitconfig provider for github
-~/.gitconfig-provider addConfig -provider=Github -key=user.email -value=$GITHUB_USER_EMAIL
-~/.gitconfig-provider addConfig -provider=Github -key=user.name -value=$GITHUB_USER_NAME
+~/gitconfig-provider addConfig -provider=Github -key=user.email -value=$GITHUB_USER_EMAIL
+~/gitconfig-provider addConfig -provider=Github -key=user.name -value=$GITHUB_USER_NAME
