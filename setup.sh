8d06591ad40ca0b87eae0229f381fa3495940ae7
PrashamTrivedi <PrashamTrivedi@users.noreply.github.com>
2023-11-10 15:55:52 +0530

    Added some useful tools in setup

--- setup.sh
+++ setup.sh
@@ -3,13 +3,19 @@
 # Install Gitconfig Provider
 
 
+# First cd into root folder
+cd ~
 curl -L "https://github.com/PrashamTrivedi/gitconfig-provider/releases/latest/download/gitconfig-provider_Linux_x86_64.tar.gz" -o "gitconfig-provider.tar.gz"
-tar -xvf gitconfig-provider.tar.gz
-chmod +x gitconfig-provider
+mkdir -p ~/gitconfig-provider
+tar -xvf gitconfig-provider.tar.gz -C ~/gitconfig-provider
+chmod +x ~/gitconfig-provider/gitconfig-provider
+sudo ln -s ~/gitconfig-provider/gitconfig-provider /usr/bin/gitconfig-provider
 
-# Copy gitconfit provider bashrc
-chmod +x gitconfig-provider-bashrc
-cp gitconfig-provider-bashrc ~/.bashrc.d/gitconfig-provider-bashrc
+curl -L "https://github.com/PrashamTrivedi/assistants-cli/releases/latest/download/assistants-cli_Linux_x86_64.tar.gz" -o "assistants-cli.tar.gz"
+mkdir -p ~/assistant-cli
+tar -xvf assistants-cli.tar.gz -C ~/assistant-cli
+chmod +x ~/assistant-cli/assistants-cli
+sudo ln -s ~/assistant-cli/assistants-cli /usr/bin/assistant-cli
 
 
 # Install and verify AWS
@@ -17,6 +23,7 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip -q awscliv2.zip
 sudo ./aws/install
 aws --version
+sudo ln -s /usr/local/aws-cli/v2/current/bin/aws /usr/bin/aws
 
 # Copy aws to Bashrc
 chmod +x awsConfig
@@ -30,3 +37,5 @@ cp git-alias ~/.bashrc.d/git-alias
 # Apply Gitconfig provider for github
 ~/gitconfig-provider addConfig --provider=Github --key=user.email --value=$GITHUB_USER_EMAIL
 ~/gitconfig-provider addConfig --provider=Github --key=user.name --value=$GITHUB_USER_NAME
+
+pip install aider-chat llm sgpt
